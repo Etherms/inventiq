9736224f02c3c7d280a20dbfb0e0753d7cd75f23
Edson Hermosa <hermosaedson459@gmail.com>
2026-07-02 22:19:04 +0800

    Fix Dashboard AppLayout import

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { computed } from 'vue'
 import { usePage } from '@inertiajs/vue3'
-import AppLayout from '../Layouts/AppLayout.vue'
+import AppLayout from '@/Layouts/AppLayout.vue'
 
 const page = usePage()
 const user = computed(() => page.props.auth.user)
